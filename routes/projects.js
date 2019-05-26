24f8ee7b02ebb256af0a17f1d104c8579b6fc238
Jonathan Nieblas <jniebla1@msudenver.edu>
2019-05-25 18:05:12 -0600

    Made some color changes to navbar. Added controllers. Connected all routes.

--- routes/projects.js
+++ routes/projects.js
@@ -1,11 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var index_controller = require('../controllers/indexController');
 var projects_controller = require('../controllers/projectsController');
-
-/* GET home page. */
-router.get('/', index_controller.index);
 
 /* GET projects page. */
 router.get('/projects', projects_controller.index);
