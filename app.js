b0f4853d1b091e2f446810853e892d8b4e17f35f
Jonathan Nieblas <jniebla1@msudenver.edu>
2019-05-26 22:15:03 -0600

    Finished with the navbar for now. Implemented sass for easier CSS.
    Next: make the html pages dynamic. Then start fleshing out each page.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var sass = require('node-sass-middleware');
 
 var indexRouter = require('./routes/index');
 var projectsRouter = require('./routes/projects');
@@ -19,6 +20,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+
+app.use(
+    sass({
+        src: path.join(__dirname, '/public/scss/'),
+        dest: path.join(__dirname, '/public/'),
+        debug: true,
+        indentedSyntax: false,
+        sourceMap: true,
+        outputStyle: 'compressed'
+    }),
+    express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/projects', projectsRouter);
